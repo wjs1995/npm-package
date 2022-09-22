24b01284815788cf02af5a46a3367ab9f51760a5
wangjinsheng <1334258066@qq.com>
2022-09-22 18:10:07 +0800

    'git-commit-code'

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# npm-package
-js通用包
+## @js-v1/date
+
+### 安装 npm i @js-v1/date
+
+### 使用
+
+ ```
+import date from '@js-v1/date'
+ 
+ date.YYYY(new Date()) 2022
+ date.YYYYMM(new Date(),'/') 2022/09
+ date.YYYYMMDD(new Date(),'/') 2022/09/22
+ date.YYYYMMDDHH(new Date(),'/') 2022/09/22 17
+ date.YYYYMMDDHHMM(new Date(),'/') 2022/09/22 17:35
+ date.YYYYMMDDHHMMSS(new Date(),'/','|') 2022/09/22 17|35|56
+ date.AddYears(new Date(), 10)  当前时间 加 10 年
+ date.AddMonths(new Date(), 10)  当前时间 加 10月
+ date.AddDays(new Date(), 10)  当前时间 加 10日
+ date.AddHours(new Date(), 10)  当前时间 加 10小时
+ date.AddMinutes(new Date(), 10)  当前时间 加 10分钟
+ date.AddSeconds(new Date(), 10)  当前时间 加 10秒
+ date.AddMilliseconds(new Date(), 10)  当前时间 加 10毫秒
+ date.AddWeeks(new Date(), 10)  当前时间 加 10周
+ ```
+
+## @js-v1/excel
+
+### 安装 npm i @js-v1/excel
+
+### 使用
+
+```
+import Xlsx from '@js-v1/excel'
+
+导出
+const x = new Xlsx()
+x.SetHeader(['a'， 'b']) // 表头
+x.SetData([[1,6],[2,6]]) // 数据二维数组
+x.SetMerges(["A2:A3"]) // 合并 A是列号后面的数字是行号
+x.Export()
+
+
+导入
+const x = new Xlsx()
+
+x.Ixport([file,file]).then(res => {
+	console.log(res);
+})
+
+性能优化 第二个参数，不计算表头
+x.Ixport([file,file], false).then(res => {
+	console.log(res);
+})
+
+性能优化 第三个参数，不计算数据
+x.Ixport([file,file], false,false).then(res => {
+	console.log(res);
+})
+```
--- js-v1/date/src/index.js
+++ js-v1/date/src/index.js
@@ -25,18 +25,56 @@ function YYYYMMDDHHMM(date = null, separator = '-', separator2 = ':') {
     return Format(date, `YYYY${separator}MM${separator}DD HH${separator2}mm`)
 }
 
-function YYYYMMDDHHMMSS(date = null) {
-    return Format(date)
+function YYYYMMDDHHMMSS(date = null, separator = '-', separator2 = ':') {
+    return Format(date, `YYYY${separator}MM${separator}DD HH${separator2}mm${separator2}ss`)
+}
+
+function AddYears(date = null, n = 0) {
+    return Moment(date).add(n, 'years').format()
+}
+
+function AddMonths(date = null, n = 0) {
+    return Moment(date).add(n, 'months').format()
+}
+
+function AddDays(date = null, n = 0) {
+    return Moment(date).add(n, 'days').format()
+}
+
+function AddHours(date = null, n = 0) {
+    return Moment(date).add(n, 'hours').format()
+}
+
+function AddMinutes(date = null, n = 0) {
+    return Moment(date).add(n, 'minutes').format()
+}
+
+function AddSeconds(date = null, n = 0) {
+    return Moment(date).add(n, 'seconds').format()
+}
+
+function AddMilliseconds(date = null, n = 0) {
+    return Moment(date).add(n, 'milliseconds').format()
+}
+
+function AddWeeks(date = null, n = 0) {
+    return Moment(date).add(n, 'weeks').format()
 }
 
 module.exports = {
-    test() {
-        console.log('调试')
-    },
     Moment,
+    Format,
     YYYYMM,
     YYYYMMDD,
     YYYYMMDDHH,
-    YYYYMMDDHHmm,
-    YYYYMMDDHHmmss
+    YYYYMMDDHHMM,
+    YYYYMMDDHHMMSS,
+    AddYears,
+    AddMonths,
+    AddDays,
+    AddHours,
+    AddMinutes,
+    AddSeconds,
+    AddMilliseconds,
+    AddWeeks
 }
